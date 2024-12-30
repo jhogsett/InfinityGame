b72266a42e2eb9e80d29b2582eca79f211df1572
Jerry Hogsett <jhogsett@gmail.com>
2024-12-30 08:59:09 -0800

    tidying

--- src/speaker.cpp
+++ src/speaker.cpp
@@ -3,28 +3,28 @@
 #include "speaker.h"
 
 void beep(int inv_freq, int pulses){
-  for(int j = 0; j < pulses; j++){
-    digitalWrite(SPEAKER_PIN, HIGH);
-    delayMicroseconds(inv_freq);
-    digitalWrite(SPEAKER_PIN, LOW);
-    delayMicroseconds(inv_freq);
-  }
+	for(int j = 0; j < pulses; j++){
+		digitalWrite(SPEAKER_PIN, HIGH);
+		delayMicroseconds(inv_freq);
+		digitalWrite(SPEAKER_PIN, LOW);
+		delayMicroseconds(inv_freq);
+	}
 }
 
 void beep_gap(int inv_freq, int pulses){
-  for(int j = 0; j < pulses; j++){
-    digitalWrite(SPEAKER_PIN, LOW);
-    delayMicroseconds(inv_freq);
-    digitalWrite(SPEAKER_PIN, LOW);
-    delayMicroseconds(inv_freq);
-  }
+	for(int j = 0; j < pulses; j++){
+		digitalWrite(SPEAKER_PIN, LOW);
+		delayMicroseconds(inv_freq);
+		digitalWrite(SPEAKER_PIN, LOW);
+		delayMicroseconds(inv_freq);
+	}
 }
 
 void beeps(int times, int inv_freq, int pulses){
-  for(int i = 0; i < times; i++){
-    beep(inv_freq, pulses);
-    beep_gap(inv_freq, pulses);
-  }
+	for(int i = 0; i < times; i++){
+		beep(inv_freq, pulses);
+		beep_gap(inv_freq, pulses);
+	}
 }
 
 void alert(int times, int gap, int beep_times, int inv_freq, int pulses){
