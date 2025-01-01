29ec000be1d9219de116f91f9d973ef1560154b8
Jerry Hogsett <jhogsett@gmail.com>
2025-01-01 12:28:53 -0800

    these should not be in source file

--- src/leds.cpp
+++ src/leds.cpp
@@ -2,17 +2,6 @@
 #include "led_handler.h"
 #include "leds.h"
 
-// LEDs handling
-#define DEFAULT_ALL_LEDS_SHOW_TIME    1000
-#define DEFAULT_ALL_LEDS_BLANK_TIME   1000
-#define DEFAULT_PANEL_LEDS_SHOW_TIME   750
-#define DEFAULT_PANEL_LEDS_BLANK_TIME  350
-#define DEFAULT_BUTTON_LEDS_SHOW_TIME  800
-#define DEFAULT_BUTTON_LEDS_BLANK_TIME 400
-#define TITLE_PANEL_LEDS_SHOW_TIME     100
-#define TITLE_PANEL_LEDS_BLANK_TIME    0
-#define TITLE_PANEL_LEDS_STYLE (LEDHandler::STYLE_PLAIN)
-
 const int led_intensities[] = {0, 0, 0, LED_INTENSITY1, LED_INTENSITY2, LED_INTENSITY3};
 LEDHandler all_leds(FIRST_LED, 6, led_intensities);
 LEDHandler panel_leds(FIRST_PANEL_LED, 3, led_intensities + 3);
