6c7368a6f1cead3ed555febc84f8205508437c74
Jerry Hogsett <jhogsett@gmail.com>
2025-01-01 14:45:31 -0800

    wip annotating

--- include/time_game.h
+++ include/time_game.h
@@ -1,9 +1,12 @@
 #ifndef __TIME_GAME_H
 #define __TIME_GAME_H
 
+// Minimum and maximum delay range for the randomly chosen delay before showing the flash
 #define MIN_DELAY 2500
 #define MAX_DELAY 5000
+// Number of time game rounds
 #define ROUNDS 3
+// Dollars won by beating the curernt time
 #define TIME_WIN 1000
 
 extern void time_game();
--- include/timeouts.h
+++ include/timeouts.h
@@ -1,7 +1,11 @@
 #ifndef __TIMEOUTS_H__
 #define __TIMEOUTS_H__
 
+// A user prompt will time out after 30 seconds
 #define PROMPT_TIMEOUT 30000L
+
+// The system will go into an idle state after 5 minutes
+// Depending on settings, it will show sleep mode or the clock
 #define IDLE_TIMEOUT 300000L
 
 #endif
