346658ba2d91cae4fb830ab8bd55278edbdb9fce
Jerry Hogsett <jhogsett@gmail.com>
2025-01-03 16:34:11 -0800

    add support for vibration motor and vibrate on long press

--- src/options_mode.cpp
+++ src/options_mode.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
+#include "motor.h"
 #include "play_data.h"
 #include "prompts.h"
+#include "speaker.h"
 #include "tools_mode.h"
 #include "utils.h"
 #include "options_mode.h"
@@ -13,7 +15,15 @@ void options_mode(){
 void options_menu(){
 	const char *labels[] = {"Off", "On", "12H", "24H", "SLP", "CLK"};
 	option_sound = toggle_prompt(FSTR("SOUND   %s"), labels, option_sound ? 1 : 0, 3, 2) ? true : false;
+
+	if(option_sound)
+		beep();
+
 	option_vibrate = toggle_prompt(FSTR("VIBRATE %s"), labels, option_vibrate ? 1 : 0, 3, 2) ? true : false;
+
+	if(option_vibrate)
+		vibrate();
+
 	option_clock_24h = toggle_prompt(FSTR("CLOCK   %s"), labels+2, option_clock_24h ? 1 : 0, 3, 2) ? true : false;
 	option_clock_on_idle = toggle_prompt(FSTR("IDLE    %s"), labels+4, option_clock_on_idle ? 1 : 0, 3, 2) ? true : false;
 	save_data();
