e59daaa3545bc993569a7c970650f1afb442aebb
Jerry Hogsett <jhogsett@gmail.com>
2025-02-19 09:32:33 -0800

    add 5 more words

--- src/word_lists.cpp
+++ src/word_lists.cpp
@@ -1,4 +1,4 @@
 #include "word_lists.h"
 
-const char *rude_words[NUM_WORDS] = {"FUCK", "SHIT", "CUNT", "COCK", "PISS", "TITS", "FART", "POOP", "DICK", "ANAL"};
-const char *nice_words[NUM_WORDS] = {"WEED", "VAPE", "BEER", "WINE", "ROSE", "GOLD", "MINT", "LOOT", "JAZZ", "SILK"};
+const char *rude_words[NUM_WORDS] = {"FUCK", "SHIT", "CUNT", "COCK", "PISS", "TITS", "FART", "POOP", "DICK", "ANAL", "ORAL", "HUMP", "SUCK", "BLOW", "PORN"};
+const char *nice_words[NUM_WORDS] = {"WEED", "VAPE", "BEER", "WINE", "ROSE", "GOLD", "MINT", "LOOT", "JAZZ", "SILK", "COIN", "SODA", "CASH", "RUBY", "GIFT"};
